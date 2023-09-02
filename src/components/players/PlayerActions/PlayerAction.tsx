4a52ffa2a70d65079560bc7a26bdc1ce7c906bab
gmontanari <gmontanari@pdxeng.ch>
2023-09-02 19:57:34 +0200

    update market close text

--- src/components/players/PlayerActions/PlayerAction.tsx
+++ src/components/players/PlayerActions/PlayerAction.tsx
@@ -1,11 +1,11 @@
 import { Player, PlayerMap, Purchase } from "../../../types/players";
 import { canMakeOffer, canReleasePlayer, getPossiblePurchaseActions } from "../../../helpers";
-import s from "./PlayerActions.module.css";
 import { marketWindow } from "../../../constants/settings";
 import { CancelOffer } from "./components/CancelOffer";
 import { ReleasePlayer } from "./components/ReleasePlayer";
 import { AcceptOffer } from "./components/AcceptOffer";
 import { MakeOffer } from "./components/MakeOffer";
+import s from "./PlayerActions.module.css";
 
 interface Props {
     player?: Player;
@@ -24,7 +24,7 @@ export const PlayerActions = ({ player, purchases, teamBudget, teamplayers, allP
     return (
         <div className={s.container}>
             {possibleActions.includes('marketClosed') && (
-                <p>Market is closed (reopen at: {marketWindow.start})</p>
+                <p className={s.marketCloseP}>Market is closed (reopen at: {marketWindow.start})</p>
             )}
             {possibleActions.includes('makeOffer') && (
                 <MakeOffer 
