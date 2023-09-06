5d612de07919ce27c1fb4574d03ffb9afadbbd14
gmontanari <gmontanari@pdxeng.ch>
2023-09-06 11:34:06 +0200

    update same player offer

--- src/components/players/PlayerActions/components/MakeOffer.tsx
+++ src/components/players/PlayerActions/components/MakeOffer.tsx
@@ -35,9 +35,11 @@ export const MakeOffer = ({ purchase, player, teamBudget, haveFreeRoleSlots }: P
       setLoading(false)
     }
 
+    const hfs = purchase?.to_team === userTeam || haveFreeRoleSlots
+
     return (
       <>
-        {haveFreeRoleSlots ? 
+        {hfs ? 
           haveBudgetForOffer ? (
             <>
               <NumberField
